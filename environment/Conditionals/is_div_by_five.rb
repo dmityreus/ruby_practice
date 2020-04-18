be61e7be93e98a43fb439c0387b7718e461fee7c
Dmitry Reus <dmitryreus@yahoo.com>
2020-04-18 01:14:41 +0000

    is div by 5

---
+++ environment/Conditionals/is_div_by_five.rb
@@ -0,0 +1,16 @@
+def is_div_by_5(number)
+    if number % 5 == 0
+        return true
+    else
+        return false
+    end
+    
+    
+end
+
+
+print is_div_by_5(40)
+puts
+print is_div_by_5(100)
+puts
+print is_div_by_5(26)
