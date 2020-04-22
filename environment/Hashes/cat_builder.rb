40add8f767bf92a3a0c141c95b8e2abeb473828d
Dmitry Reus <dmitryreus@yahoo.com>
2020-04-22 07:06:40 +0000

    Hashes Practice

---
+++ environment/Hashes/cat_builder.rb
@@ -0,0 +1,15 @@
+def cat_builder (name, color, age)
+    
+    return { "name" => name, "color" => color, "age" => age  }
+    
+    
+    
+end
+    
+    
+    
+    print cat_builder("Whiskers", "orange", 3) #=> {"name"=>"Whiskers", "color"=>"orange", "age"=>3}
+puts
+
+print cat_builder("Salem", "black", 100) #=> {"name"=>"Salem", "color"=>"black", "age"=>100}
+puts
